0b983ec86017038eb2804b29337c0c3f579db790
friz2323 <145427748+friz2323@users.noreply.github.com>
2023-09-19 13:30:18 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0x46b337b9a5c3c74834fcd9a23e0f9810f57b3d0af6fa17ea1c39634a8d9b5ef3"], //Your private key starting with "0x" 
+      accounts: ["MY_PK"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
